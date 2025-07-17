e911a86aad9b3004ed14289e0a66cecf02046787
ktran151 <ktranman@yahoo.com>
2025-07-16 22:07:04 -0400

    adding surroundings and exits scanning methods

--- Explorer.java
+++ Explorer.java
@@ -4,7 +4,36 @@ import java.util.Arrays;
 
 public class Explorer {
 
-public static void main(String[] args) {
-   
-}
+/* Given the coordinates of the player, returns adjacent rooms
+ * as [N, E, S, W]. If player is adjacent to the edge, int will be 4
+ * 
+ */
+   public static int[] surroundings(int xCoord, int yCoord, int[][] grid){
+      int[] result = new int[4];
+      result[1] = grid[xCoord][yCoord + 1]; // N side
+      result[2] = grid[xCoord + 1][yCoord]; // E side
+      result[3] = grid[xCoord][yCoord - 1]; // S side
+      result[4] = grid[xCoord - 1][yCoord]; // W side
+      return result;
+   }
+
+   /*
+    * Given the coordinates of the player, returns adjacent rooms that are available 
+    * as [N, E, S, W]. If 
+    */
+   public static boolean[] exits(int xCoord, int yCoord, int max){
+      boolean[] result = {true, true, true, true}; //assumes player can move in all directions
+      if (yCoord == 0) { result[1] = false; }   // N side
+      if (xCoord == max) { result[2] = false; } // E side
+      if (yCoord == max){ result[3] = false; }  // S side
+      if (xCoord == 0){ result[4] = false; }    // W side
+
+      return result;
+   }
+
+   public static void main(String[] args) {
+      boolean[] direction = exits(0, 0, 0);
+      System.out.println(Arrays.toString(direction));
+      
+   }
 }
