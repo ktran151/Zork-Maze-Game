e64a7bb031b01d72aab5b5fcac98af56591088a3
ktran151 <ktranman@yahoo.com>
2025-07-16 22:51:36 -0400

    completed surroundings and exits methods descriptions along incorporating exits array to surroundings to get the correct output.

--- Explorer.java
+++ Explorer.java
@@ -5,21 +5,43 @@ import java.util.Arrays;
 public class Explorer {
 
 /* Given the coordinates of the player, returns adjacent rooms
- * as [N, E, S, W]. If player is adjacent to the edge, int will be 4
- * 
+ * as [N, E, S, W]. If player is adjacent to the edge, int will be 4.
+ * checks through exits array to avoid edge cases. Hard-coded
  */
-   public static int[] surroundings(int xCoord, int yCoord, int[][] grid){
+   public static int[] surroundings(int xCoord, int yCoord, int[][] grid, boolean[] exits){
       int[] result = new int[4];
-      result[1] = grid[xCoord][yCoord + 1]; // N side
-      result[2] = grid[xCoord + 1][yCoord]; // E side
-      result[3] = grid[xCoord][yCoord - 1]; // S side
-      result[4] = grid[xCoord - 1][yCoord]; // W side
+
+      if(exits[1]){
+         result[1] = grid[xCoord][yCoord + 1]; // N side
+      } else {
+         result[1] = 4;
+      }
+
+      if(exits[2]){
+         result[2] = grid[xCoord + 1][yCoord]; // E side
+      } else{
+         result[2] = 4;
+      }
+
+      if (exits[3]){
+         result[3] = grid[xCoord][yCoord - 1]; // S side
+      } else {
+         result[3] = 4;
+      }
+         
+      if (exits[4]){
+         result[4] = grid[xCoord - 1][yCoord]; // W side
+      } else {
+         result[4] = 4;
+      }
+      
       return result;
    }
 
    /*
     * Given the coordinates of the player, returns adjacent rooms that are available 
-    * as [N, E, S, W]. If 
+    * as [N, E, S, W]. If able to move that direction, then will return true 
+    * in the corresponding slot.
     */
    public static boolean[] exits(int xCoord, int yCoord, int max){
       boolean[] result = {true, true, true, true}; //assumes player can move in all directions
